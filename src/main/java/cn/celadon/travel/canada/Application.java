972f29e38a04f5ae54edce6ba9c50f8afd069da7
empqqty <yi.b.zhang@ericsson.com>
2017-05-15 17:25:45 +0800

    1) change the project structure 2) improve index page ui 3) design the second level page layout 4) design the news display page

---
+++ src/main/java/cn/celadon/travel/canada/Application.java
@@ -0,0 +1,28 @@
+package cn.celadon.travel.canada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by empqqty on 5/11/2017.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
